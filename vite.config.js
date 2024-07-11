c255c42dc55f05f1613892f4d77f351b314bd599
Christopher <odeniyichristopher7@gmail.com>
2024-07-11 09:26:05 +0100

    fix configuration to  vite to support external firebase

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-// vite.config.js
-export default {
+  // vite.config.js
   build: {
     rollupOptions: {
       external: ['firebase']
     }
   }
-}
+})
+
