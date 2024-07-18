7c2d7d9ce2ff63c7ea40bc2fe4b0ed84ffbdb692
Christopher <odeniyichristopher7@gmail.com>
2024-07-18 18:10:05 +0100

    cart icon number count and item sent to cart function

--- src/components/molecules/CartItem.jsx
+++ src/components/molecules/CartItem.jsx
@@ -1,9 +1,28 @@
 const CartItem = ({cartItem}) => {
-    const {productname,quantity} = cartItem;
+    const {productname,img, description, price, quantity} = cartItem;
     return (
         <div>
-             <h1>{productname}</h1>
-            <span>{quantity}</span>
+             <div className="flex md:h-[8rem] justify-between items-center px-[1rem]  border-[1px] rounded-[0.5rem]">
+             <div className="flex justify-center items-center gap-1 text-[#000] w-[10%] px-2">
+                <span>({quantity})</span>
+                <span>X</span>
+             </div>
+                            <div className="md:w-[7rem] md:h-[7rem] w-[7rem] h-[8rem]  ">
+                                <img className="flex items-center object-contain w-full h-full " src={img} alt={`${productname}`}/>
+                            </div>
+                            <div className="flex flex-col justify-between md:py-[1rem] py-[1rem] text-[#000] w-[70%]">
+                                <div>
+                                    <h3 className="md:text-[1.5rem] text-[1rem] font-[garamond] leading-[1.8125rem] capitalize">
+                                    {productname}
+                                    </h3>
+                                    <p className="text-[0.7rem]">{description}</p>
+                                </div>
+                                <div>
+                                    <p className="md:text-[0.9rem] text-[1rem] font-[garamond] leading-[1.5rem]">{price}<sup className="md:text-[0.7rem] ">.00</sup></p>
+                                </div>                                
+                            </div>
+                           
+                        </div>
         </div>
     )
 }
